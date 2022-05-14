a590770028fd85142e34a5b81a631b7c810dae15
Jannis Baum <jannis.baum@gmail.com>
2022-05-14 23:24:35 +0200

    hid echo and basic oled print

--- keyboards/crkbd/keymaps/jannis/rules.mk
+++ keyboards/crkbd/keymaps/jannis/rules.mk
@@ -1,7 +1,10 @@
-BOOTLOADER      = atmel-dfu
+BOOTLOADER = atmel-dfu
+LTO_ENABLE = yes
 
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+RAW_ENABLE = yes
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
